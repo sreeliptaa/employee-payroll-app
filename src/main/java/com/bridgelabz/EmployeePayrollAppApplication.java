5c792134031197da75fb38c49fec0dd047417465
sreeliptaa <sreelipta65@gmail.com>
2021-12-14 06:16:00 +0530

    Added : EmployeePayrollApplication class after modified

--- src/main/java/com/bridgelabz/EmployeePayrollAppApplication.java
+++ src/main/java/com/bridgelabz/EmployeePayrollAppApplication.java
@@ -3,10 +3,17 @@ package com.bridgelabz;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Purpose : To simulate Employee Payroll Application
+ *
+ * @author : SREELIPTA
+ * @since : 06-12-2021
+ */
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
 	}
 
