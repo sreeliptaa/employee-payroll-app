abaffb98d3f1f5a040ceb11e84897e03b224407f
sreeliptaa <sreelipta65@gmail.com>
2021-12-18 15:06:51 +0530

    Added : interface IEmployeePayroll to hide the implementation in the employee payroll application

--- src/main/java/com/bridgelabz/service/IEmployeePayrollService.java
+++ src/main/java/com/bridgelabz/service/IEmployeePayrollService.java
@@ -1,2 +1,24 @@
-package com.bridgelabz.service;public class IEmployeePayrollService {
+package com.bridgelabz.service;
+
+import com.bridgelabz.dto.EmployeeDto;
+import com.bridgelabz.entity.EmployeeEntity;
+
+import java.util.List;
+
+/**
+ * Purpose : This is the interface of employee payroll service by which all the process of service class will occur
+ *
+ * @author SREELIPTA
+ * @since 2021-12-11
+ */
+public interface IEmployeePayrollService {
+    List<EmployeeDto> getListOfAllEmployee();
+
+    EmployeeEntity findEmployeeById(int id);
+
+    String addEmployee(EmployeeDto employeeDto);
+
+    String updateEmployee(int id, EmployeeDto employeeDto);
+
+    String deleteEmployee(int id);
 }
